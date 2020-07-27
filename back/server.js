ed42760ffb9447895651c01953244f1b75cbd6bd
Dimaggio224 <dima224xz@gmail.com>
2020-07-27 19:24:08 +0300

    a

--- back/server.js
+++ back/server.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/project', {useNewUrlParser: true});
+var Schema = mongoose.Schema;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+
+
+
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({msg: 'hello'}));

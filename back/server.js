392e9c308417e89ee6a23d96a4c842cec95902a4
Dimaggio224 <dima224xz@gmail.com>
2020-07-27 22:56:15 +0300

    a

--- back/server.js
+++ back/server.js
@@ -2,13 +2,40 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/project', {useNewUrlParser: true});
+
+try {
+    mongoose.connect('mongodb://localhost/project', {useNewUrlParser: true});
+} catch (error) {
+    handleError(error);
+}
 var Schema = mongoose.Schema;
+
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
+var userSchema = new Schema({
+    nick:  String,
+    email: String,
+    password: String,
+    avatar: String,
+    active: Boolean,
+    admin: Boolean
+    }
+);
+var User = mongoose.model('User', userSchema);
 
+
+var postSchema = new Schema({
+    userId:  String,
+    title: String,
+    text: String,
+    active: Boolean
+    }
+);
+var Post = mongoose.model('Post', postSchema);
 
 
 
